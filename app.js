cfde4a726033d9cf979cb75bb01df748626d0fdb
Terver <tervenda18@gmail.com>
2023-03-23 06:35:33 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const animals = require("./animals");
+const partners = require("./partners");
+
+app.use(cors());
+
+app.get("/data/animals", (req, res) => {
+  let newAnimals = animals.map((animal) => {
+    const { name, img } = animal;
+    return { name, img };
+  });
+  res.status(200).json(newAnimals);
+});
+
+app.get("/data/animals/:id", (req, res) => {
+  const { id } = req.params;
+  let animal = null;
+  animal = animals.find((animal) => animal.id === Number(id));
+
+  if (animal) {
+    return res.status(200).json(animal);
+  } else {
+    return res
+      .status(404)
+      .json({ status: "failure", message: `No animal found with id ${id}` });
+  }
+});
+
+app.get("/data/partners", (req, res) => {
+  res.status(200).json(partners);
+});
+
+app.get("*", (req, res) => {
+  res.status(404).json({ status: "Failure", message: "Resource not found" });
+});
+
+app.listen(5000, () => console.log("Listening on port 5000..."));
